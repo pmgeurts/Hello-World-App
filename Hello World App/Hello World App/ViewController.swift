e52e323c49f2ff39e7e0f6798347d22dc2b16687
Paul Geurts <pmgeurts@gmail.com>
2017-05-17 16:25:59 +0200

    Upload first project files

---
+++ Hello World App/Hello World App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hello World App
+//
+//  Created by Paul Geurts on 17/05/2017.
+//  Copyright © 2017 PG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
